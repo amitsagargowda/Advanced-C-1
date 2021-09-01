f57e33ac38ca89dea77422a07bad0a3df3fc6cb8
abinashprabakar <abinashprabakaran@gmail.com>
2021-09-01 12:49:42 +0530

    Updated

--- fileoperation/file_read_write.c
+++ fileoperation/file_read_write.c
@@ -1,30 +1,30 @@
 /* This program explains about how a basic file operation is executed.
  * In this program a file is opened in read | write mode and a string is copied to the existing file.
  *
+ * Email : abinashprabakaran@gmail.com
+ * Date : 31.08.2021
  * Author : Abinash
  */
 
 
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<string.h>
-#include<unistd.h>
-#include<errno.h>
+#include<stdio.h>	/* required for printf */
+#include<fcntl.h>	/* manipulate file descriptor */
+#include<string.h>	/* required for string functions */
+#include<unistd.h>	/* required for write, read, close */
+#include<errno.h>	/* required for strerror */
 
 int main()
 {
 	int fd, read_bytes;
 	char buff[256];
-	fd = open("test",O_RDWR);	//test file is opened in Read | Write mode
+	fd = open("test",O_RDWR);	/* test file is opened in Read | Write mode */
 	if(fd < 0)
 	{
 		perror("Error opening file\n");	
 		return 0;
 	}
-	printf("FD (%d)\n",fd);
-	read_bytes = read(fd,buff,40);
+	printf("FD (%d)\n",fd);	/* fd contents */
+	read_bytes = read(fd,buff,40);	/* read up to count bytes from file descriptor into the buffer starting at buf */
 	if(read_bytes < 0)
 	{
 		printf("Read error\n");
@@ -32,11 +32,12 @@ int main()
 		return 0;
 	}
 	printf("File contents\n");
-	printf("%s\n",buff);
-	strcpy(buff,"testing_write_system_call");	//new string is copied to buff
+	printf("%s\n",buff);	/* print file contents */
+	strcpy(buff,"testing_write_system_call");	/* new string is copied to buff */
 
-	read_bytes = write(fd,buff,40);			
+	/* writes up to count bytes from the buffer starting at buf to the file referred to by the file descriptor fd */
+	read_bytes = write(fd,buff,40);
 	if(read_bytes < 0)
-		printf("Write : %s\n",strerror(errno));
-	close(fd);
+		printf("Write : %s\n",strerror(errno)); /* strerror ->  returns the appropriate error or unkown error */
+	close(fd);	/* close the file */
 }
