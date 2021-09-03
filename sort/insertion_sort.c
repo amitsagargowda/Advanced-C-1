af7ca266e6f6e189d4b99a1cb23f704032c6e14e
abinashprabakar <abinashprabakaran@gmail.com>
2021-09-03 13:24:13 +0530

    Updated

--- sort/insertion_sort.c
+++ sort/insertion_sort.c
@@ -1,4 +1,14 @@
-#include<stdio.h>
+/* This program explains about insertion sort.
+ * Explaination about the insertion sort is given in README.md file
+ *
+ * Email : abinashprabakaran@gmail.com	
+ * Date : 02.09.2021
+ * Author : Abinash
+ */
+
+#include<stdio.h>	/* required for printf and scanf */
+
+/* main program */
 
 int main()
 {
@@ -10,24 +20,28 @@ int main()
 
 	printf("Enter %d elements\n",n);
 	for(i=0; i<n; i++)
-		scanf("%d",&a[i]);
+		scanf("%d",&a[i]);	/* storing 'n' number of elements in a[i] */
 
-	for(i=1; i<=n-1; i++)
+	for(i=0; i<n; i++)
 	{
-		j = i;
-		while(j>0 && a[j-1] > a[j])
+		j = i;			/* assign 'i' to 'j' */
+
+		while(j>0 && a[j-1] > a[j])	/* comparing min element with the other elements   */
 		{
+			/* swapping operations */
+
 			temp = a[j];
 			a[j] = a[j-1];
 			a[j-1] = temp;
-			j--;
+
+			j--;		/* decrementing j value */
 		}
 	}
 
 	printf("After Insertion sort : \n");
 	for(i=0; i<n; i++)
-		printf("%d  ",a[i]);
+		printf("%d  ",a[i]);	/* prints the sorted array */
 	printf("\n");
 
-	return 0;
+	return 0;			/* program executed successfully */
 }
