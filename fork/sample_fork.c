2410cdeec55935c2675601e3537e04c045f145b7
abinashprabakar <abinashprabakaran@gmail.com>
2021-09-03 09:52:22 +0530

    Updated

--- fork/sample_fork.c
+++ fork/sample_fork.c
@@ -13,7 +13,6 @@
 
 int main()
 {
-
 	fork();			/* Creates child process by duplicating the parent process */
 	printf("Hello\n");	/* Hello is printed in the child process as well as the parent process */
 	return 0;		/* return 0 means program executed successfully */
