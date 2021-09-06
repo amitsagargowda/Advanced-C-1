b7cad19167d8d78ddd662a753637c58338b8fd87
abinashprabakar <abinashprabakaran@gmail.com>
2021-09-06 12:12:08 +0530

    Updated

--- DSA/linked_list/single_linked_list/save.c
+++ DSA/linked_list/single_linked_list/save.c
@@ -3,9 +3,10 @@ void save(ST *ptr)
 {
       FILE *fp = fopen("data.txt","w");
 
-      while(ptr != 0) {
-      fprintf(fp,"%d %s %f\n",ptr->roll,ptr->name,ptr->marks);
-      ptr = ptr->next;
+      while(ptr != 0) 
+      {
+	      fprintf(fp,"%d %s %f\n",ptr->roll,ptr->name,ptr->marks);
+      	      ptr = ptr->next;
       }
 }
 
